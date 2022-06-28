48e4d2079e549cb0954a0aa061a79f2406143cac
SebasDuranona <sduranona97@gmail.com>
2022-06-28 17:05:36 -0500

    Program to test if/else conditions

---
+++ Basics/password_1.py
@@ -0,0 +1,27 @@
+# Simple program to verify a password
+password = 'password'
+
+print ('Hello, welcome to this app')
+print ()
+print ('-'*10)
+print ('Can I have a name?')
+
+userName = input()
+
+print ('Nice to meet you, ' + userName)
+print ()
+print ('You see, ' + userName + '... We are not a trustfull bunch...')
+print ('I need to confirm you are who you say you are')
+print ('PASSWORD???')
+
+userPass = input()
+
+print('-'*10)
+
+if (userPass==password):
+    print ('Did not doubt you for a second')
+    print()
+    print('Welcome to the Criminal Den!')
+
+else:
+    print ('No chance in hell you are coming in!')
