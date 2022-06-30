8bce8a66212944fb989b79d74447cded8bdd1da6
SebasDuranona <sduranona97@gmail.com>
2022-06-29 21:29:02 -0500

    created the collatz.py program

---
+++ Basics/collatz.py
@@ -0,0 +1,19 @@
+# Program to perform the Collatz Sequence on any integer
+
+def collatz(number):
+
+    if (number % 2 == 0): # Check if number is even
+        print (number // 2)
+        return (number // 2)
+    elif (number % 2 == 1): # Check if number is odd
+        print (3 * number + 1)
+        return (3 * number + 1)
+
+# User Interaction
+
+print ('Enter a number')
+numberInput = int(input())
+
+while numberInput != 1:
+    numberInput = collatz(numberInput)
+
