929cc330b1a4ac00c9a04f982da916133af770aa
Sebastian Gomez-Duranona <sduranona97@gmail.com>
2022-07-07 20:15:58 -0500

    Delete mclip.bat

--- Basics/Multi-Clipboard_Project/mclip.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\sdura\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\sdura\Documents\Github_Projects\Python_Automation\Basics\Multi-Clipboard_Project\mclip.py" %*
-@pause
